55501148860b27d9e3fc98719fb9353b56e50603
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-01-20 19:34:03 -0600

    Website Rebuilt under subdirectory 'Oops'

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,152 @@
 var startButton = document.getElementById("startButton");
+var startCard = document.getElementById("startCard");
+var cardContainer = document.getElementsByClassName("cardContainer");
+var userScore = 0;
+var timerElement = document.getElementById("timer");
+var countdownInterval
+var timeLeft = 60;
 
-startButton.addEventListener("click", function() {
-    
-})
+var trueButton = document.querySelector(".trueButton");
+var falseButton = document.querySelector(".falseButton");
+var cardText = document.querySelector("p");
+var cardHeader = document.querySelector("#cardHeader");
+var buttonOne = document.querySelector("#buttonOne");
+var buttonTwo = document.querySelector("#buttonTwo");
+var buttonThree = document.querySelector("#buttonThree");
+var buttonFour = document.querySelector("#buttonFour");
+var alertText = document.querySelector("#alertText");
+
+function updateCountdown() {
+    if (timeLeft > 0) {
+        timeLeft --;
+        timerElement.textContent = "Time remaining: " + timeLeft;
+    } else {
+        clearInterval(countdownInterval);
+        timerElement.textContent = "Time's up!";
+    };
+};
+
+function handleTrue() {
+    userScore += 20;    
+    console.log(userScore);
+};
+
+function handleFalse() {
+    timeLeft -= 10;
+    updateCountdown();
+};
+
+/* Start the quiz and begin the quiz */
+startButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    countdownInterval = setInterval(updateCountdown, 1000);
+    firstQuestion();
+});
+
+function firstQuestion() {
+    var donaldFact = "As of the release of Final Fantasy XVI, Bahamut reclaimed his crown.";
+
+    startButton.style.display = "none"
+    cardText.style.display = "none"; 
+
+    cardHeader.textContent = "Donald Duck is a more powerful mage than Bahamut:";
+    buttonOne.textContent = "True";
+    buttonTwo.textContent = "False";
+
+    buttonOne.style.display = "block";
+    buttonTwo.style.display = "block";
+
+
+    trueButton.addEventListener("click", function() {
+        handleTrue();
+        secondQuestion();
+        alert("Correct! " + donaldFact);
+    }, {once:true});
+    falseButton.addEventListener("click", function() {
+        handleFalse();
+        secondQuestion();
+        alert("Incorrect! " + donaldFact);
+        alert("-10 seconds");
+    }, {once:true});
+};
+
+function secondQuestion() {
+    var leonFact = "Originally named 'Squall', this Final Fantasy VIII protagonist was renamed 'Leon' in 2003's 'Kingdom Hearts'";
+    buttonOne.classList.replace("trueButton", "falseButton");
+    buttonThree.classList.replace("falseButton", "trueButton");
+
+    cardHeader.textContent = "What is the name of the character who wields a Gunblade?";
+    buttonOne.textContent = "Squall"
+    buttonTwo.textContent = "Cloud" 
+    buttonThree.textContent = "Leon";
+    buttonFour.textContent = "Clive";
+
+    buttonThree.style.display = "block";
+    buttonFour.style.display = "block";
+
+    trueButton = document.querySelector(".trueButton");
+    falseButton = document.querySelector(".falseButton");
+
+    trueButton.addEventListener("click", function() {
+        handleTrue();
+        thirdQuestion();
+        alert("Correct! " + leonFact);
+    }, {once:true});
+    falseButton.addEventListener("click", function() {
+        handleFalse();
+        thirdQuestion();
+        alert("Incorrect! " + leonFact);
+        alert("-10 seconds");
+    }, {once:true});
+};
+
+function thirdQuestion() {
+    var bbbFact = "";
+    buttonThree.classList.replace("trueButton", "falseButton");
+    buttonTwo.classList.replace("falseButton", "trueButton");
+
+    cardHeader.textContent = "What is the title of the game immediately proceeding Kingdom Hearts III?";
+    buttonOne.textContent = "Kingdom Hearts 3D: Dream Drop Distance"
+    buttonTwo.textContent = "Kingdom Hearts 0.2: A Fragmentary Passage" 
+    buttonThree.textContent = "Kingdom Hearts Squared: Pages Beyond the End";
+    buttonFour.textContent = "Epic Mickey";
+
+    trueButton = document.querySelector(".trueButton");
+    falseButton = document.querySelector(".falseButton");
+
+    trueButton.addEventListener("click", function() {
+        handleTrue();
+        fourthQuestion();
+        alert("Correct! " + bbbFact);
+    }, {once:true});
+    falseButton.addEventListener("click", function() {
+        handleFalse();
+        fourthQuestion();
+        alert("Incorrect! " + bbbFact);
+        alert("-10 seconds");
+    }, {once:true});
+};
+
+function fourthQuestion() {
+    buttonTwo.classList.replace("trueButton", "falseButton");
+    buttonOne.classList.replace("falseButton", "trueButton");
+
+    cardHeader.textContent = "What game immediately follows Kingdom Hearts III?";
+    buttonOne.textContent = "Kingdom Hearts: Melody of Memory"
+    buttonTwo.textContent = "Kingdom Hearts: Dark Road" 
+    buttonThree.textContent = "Kingdom Hearts 3d: Dream Drop Distance";
+    buttonFour.textContent = "Sudoku";
+
+    trueButton = document.querySelector(".trueButton");
+    falseButton = document.querySelector(".falseButton");
+
+    trueButton.addEventListener("click", function() {
+        handleTrue();
+        lastQuestion();
+    }, {once:true});
+    falseButton.addEventListener("click", function() {
+
+        handleFalse();
+        lastQuestion();
+    }, {once:true});
+};
---
+++ Oops/script.js
@@ -0,0 +1,138 @@
+var quizEl = $(".quiz");
+var quizHeader = $("<h2></h2>").text("Kingdom Hearts Quiz");
+var quizText = $("<p></p>").text("Can you find all the correct answers before time runs out?")
+var startButton = $("<button></button>").addClass("startButton").text("Start Quiz");
+var timer = 60;
+var timerEl = $("<h2></h2>").text(timer)
+var currentQuestion = 0;
+var playerScore = 0;
+
+/* Hold the values for each question */
+var quizEntries = [
+  {
+    question: "Who is the most powerful mage in Kingdom Hearts?",
+    choices: ["Aqua", "Yen Sid", "Donald Duck", "Merlin"],
+    correctAnswer: "Donald Duck",
+    alertText: "In Kingdom Hearts III, Donald Duck performs the spell 'Zetaflare', making him more powerful than even the Dragon god, Bahamut, at the time of release!"
+  },
+  {
+    question: "Which character was NOT 'Norted'?",
+    choices: ["Larxene", "Riku", "Aqua", "Xigbar"],
+    correctAnswer: "Aqua",
+    alertText: "Though she temporarily lost herself to the darkness, Aqua was never one of Xehanort's vessels."
+  },
+  {
+    question: "Which character does Sora first encounter in Traverse Town?",
+    choices: ["Leon", "Cloud", "Squall", "Axel"],
+    correctAnswer: "Leon",
+    alertText: "Originally called 'Squall' in Final Fantasy VIII, Leon was renamed in Kingdom Hearts."
+  },
+  {
+    question: "What is the identity Master of Masters?",
+    choices: ["Sora", "Demyx", "Luxord", "None of the Above"],
+    correctAnswer: "None of the Above",
+    alertText: "Though there are many theories on the identity of the Master of Masters, it has not yet been revealed."
+  },
+  {
+    question: "How many Kingdom Hearts games have been released as of January 2024?",
+    choices: ["13", "24", "5", "3"],
+    correctAnswer: "13",
+    alertText: "According to Wikipedia, there have been 13 Kingdom Hearts titles released to date."
+  },
+];
+
+function highScoresList() {
+  quizEl.empty()
+  confirm("Build 'highScoreList' function");
+}
+
+function endGame() {
+  timerEl.empty();
+  quizEl.empty();
+  var highScores = $("<h2></h2>").text("Highscores");
+  quizEl.append(highScores);
+  userInput = $("<input></input>")
+    .attr("placeholder", "Player Initials")
+    .attr("input", "text")
+    .val("");
+  userInput = userInput;
+  quizEl.append(userInput);
+  var submitScore = $("<button></button>").text("Submit");
+  quizEl.append(submitScore);
+
+  submitScore.click(function() {
+    console.log(userInput.val() + " - " + playerScore + "/5");
+  });
+  highScoresList();
+};
+
+function startTimer() {
+  confirm("Build 'startTimer' function")
+};
+
+function updateTimer() {
+  confirm("Build 'updateTimer' function!");
+};
+
+/* Update player score and display alert */
+function handleCorrect() {
+  alert("Correct!" + "\n\n" + quizEntries[currentQuestion].alertText);
+  playerScore++;
+  console.log(playerScore);
+  currentQuestion++;
+  if (currentQuestion < quizEntries.length && timer > 0) {
+    generateQuiz();
+  } else {
+    endGame();
+  };
+};
+
+/* Update timer and display alert */
+function handleIncorrect() {
+  alert("Incorrect!" + "\n\n" + quizEntries[currentQuestion].alertText);
+  playerScore = playerScore;
+  console.log(playerScore);
+/*   timer -= 10;
+  console.log(timer); */
+  currentQuestion++;
+  if (currentQuestion < quizEntries.length && timer > 0) {
+    generateQuiz();
+  } else {
+    endGame();
+  };
+};
+
+/* Generate ethe question elements and text */
+function generateQuiz() {
+  quizEl.empty();
+
+  quizHeader.text(quizEntries[currentQuestion].question);
+  quizEl.append(quizHeader);
+  var correctAnswer = quizEntries[currentQuestion].correctAnswer;
+    
+  quizEntries[currentQuestion].choices.forEach(function(choice) {
+    var quizChoices = $("<button></button>")
+      .text(choice)
+      .click(function() {
+        if (quizChoices.text() == correctAnswer) {
+          handleCorrect();
+          } else {
+          handleIncorrect();
+        };
+      });
+      quizEl.append(quizChoices);
+  });
+};
+
+/* Create elements and begin the quiz */
+$(function() {
+  quizEl.append(quizHeader);
+  quizEl.append(quizText);
+  quizEl.append(startButton);
+
+  startButton.click(function() {
+    startTimer();
+    $(".startButton").hide();
+    generateQuiz();
+  });
+});
